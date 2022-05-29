061d9c47847d7815058904b03b11f9b570d0ddfe
genissubils <99212813+genissubils@users.noreply.github.com>
2022-05-29 13:10:04 +0200

    Add files via upload

---
+++ s-textui/fxmanifest.lua
@@ -0,0 +1,24 @@
+fx_version "adamant"
+
+game       "gta5"
+
+lua54      "yes"
+
+
+ui_page {
+    "html/index.html"
+}
+
+files {
+    "html/index.html",
+    "html/style.css",
+    "html/script.js"
+}
+
+client_scripts {
+    "client/main.lua",
+}
+
+exports {
+	"showHelp"
+}
